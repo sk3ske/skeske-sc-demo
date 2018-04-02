3857a0316f75982c337385ef81d38f8889a8fb7b
Stephanie Keske <skeske@gmail.com>
2018-04-01 21:33:09 -0700

    implmenting semantic-ui-react, structuring RepoCard as a Card component and adding general ui polish

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-This is a Crazy Cool C-C-Crypto Code Corroborator!
+Simple React app to query the GitHub API for public repo information on the top six cryptocurrencies by market cap (configured manually).
 
-It is built on top of [Create React App](https://github.com/facebookincubator/create-react-app).
+It is built on top of [Create React App](https://github.com/facebookincubator/create-react-app) and utilizes [React Semantic UI](https://react.semantic-ui.com/).
+
+Settings
+-
+Inside the `src` directory, you'll find `crypto-repo-list.json`. This file contains an array of objects with the `user` and `repo` of the top 6 crypto repos by market cap (as originally determined using https://coinmarketcap.com/). These two parameters are required by the GitHub API /repos/ endpoint.
+
+To add more repos to the page or remove some from the existing display, simply add/remove a corresponding object (with `user` and `repo` name/value pairs) and rebuild the app.
 
 Usage
 -
-To *run* the app in development mode, run `npm start` inside the top-level folder. Open http://localhost:3000 to view it in a browser.
+To *run* the app in development mode, run the command `npm start` inside the top-level folder. Open http://localhost:3000 to view it in a browser.
 
 To *deploy* the app, use the command `npm run build`, which will build the app to the `build` folder. This will bundle React in production mode and optimize for performance.
 
-For more information, visit the [Create React App] docs:
+For more information, visit the create-react-app docs:
 > https://github.com/facebookincubator/create-react-app#npm-start-or-yarn-start
--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
-import RepoCardContainer from './RepoCardContainer.js';
+import RepoCardContainer from './containers/RepoCardContainer.js';
 import repoList from './crypto-repo-list.json';
+import { Header, Container, Card, Icon, Divider } from 'semantic-ui-react'
 
 class App extends Component {
 
   render() {
-
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Crypto is COOL</h1>
-          <h5>
-            Top 6 Cryptocurrencies by market share and their associated largest GitHub repos - <i> current as of 4/1/2018</i>
-          </h5>
-        </header>
-        {repoList.map((obj, index) =>
-          <RepoCardContainer queryData={obj} key={index}/>
-        )}
-      </div>
+
+        <div className="App-content">
+          <Header attached='top'
+                  size='medium'
+                  inverted />
+
+          <Header as='h2' icon>
+            <Icon name='github'
+                  color='grey'/>
+            Cryptocurrencies on GitHub
+            <Header.Subheader>
+              Most prominent repositories of the top 6 Cryptocurrencies by market cap.
+              <br />
+              <i>Source: <a href="https://coinmarketcap.com/">coinmarketcap</a> - current as of 4/1/2018</i>
+            </Header.Subheader>
+          </Header>
+
+          <Divider hidden />
+
+          <Container>
+            <Card.Group centered>
+              {repoList.map((obj, index) =>
+                <RepoCardContainer queryData={obj} key={index}/>
+              )}
+            </Card.Group>
+          </Container>
+
+          <Divider hidden />
+        </div>
+
+      <Header attached='bottom' size='medium' inverted >
+        created by <a href="http://www.skeske.com">skeske</a> using <a href="https://reactjs.org/">react</a> and <a href="https://react.semantic-ui.com/">semantic-ui</a>
+    </Header>
+
+    </div>
     );
   }
 }
